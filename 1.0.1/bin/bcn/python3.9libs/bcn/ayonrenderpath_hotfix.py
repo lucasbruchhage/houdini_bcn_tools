a7c4fb78a67d403339e3172b1fbd0e07a4d8a0c8
lucasbruchhage <lucas.bruchhage@bcnvisuals.com>
2024-10-14 13:40:19 +0000

    logo and path fixer

---
+++ 1.0.1/bin/bcn/python3.9libs/bcn/ayonrenderpath_hotfix.py
@@ -0,0 +1,22 @@
+import hou
+
+def go():
+    # Get the selected nodes
+    selected_nodes = hou.selectedNodes()
+
+    # Iterate through each selected node
+    for node in selected_nodes:
+        node.parm("RS_archive_file").set("$HIP/pyblish/rs/$OS/$OS.$F4.rs")
+        node.parm("RS_outputFileNamePrefix").set("$HIP/pyblish/renders/$OS/$OS.$AOV.$F4.exr")
+        node.parm("variant").set("$OS")
+        node.parm("AYON_productName").set("$OS")
+        node.parm("folderPath").set("$AYON_FOLDER_PATH")
+        node.parm("task").set("$AYON_TASK_NAME")
+
+        # Add other parameters you want to set
+
+    # Notify that parameters have been set
+    hou.ui.displayMessage("Parameters have been set!")
+
+# Call the function
+go()
